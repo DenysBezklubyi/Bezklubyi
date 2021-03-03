6dd06fe3577b3ebbf256a41eefe95fee3b227188
DenysBezklubyi <besklubyi@gmail.com>
2021-03-03 21:31:49 +0200

    lesson19

---
+++ homework/lesson19/script.js
@@ -0,0 +1,25 @@
+//прямокутник
+
+var ab = 5;
+var cd = 8;
+var s = ab*cd;
+console.log(' площа прямокутника '  + s )
+
+// коло
+var p = 3.14;
+var r = 4;
+var s = p * r**2;
+
+console.log(' площа кола ' + s)
+
+//степінь
+var a = 7;
+var n = 5;
+var an= a*n;
+console.log(' степінь ' + an)
+
+// PI
+
+const PI = 3.14
+
+console.log(' pi ' + PI)
